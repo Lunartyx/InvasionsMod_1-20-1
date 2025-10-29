b71b9bfab42c67b32a16d67229e478858c7ab0f9
Lunartyx <dominik@urbanetz.li>
2025-10-29 15:47:48 +0100

    Updated binary files

--- src/main/java/com/lunartyx/invasionmod/client/render/RiftZombiePiglinEntityRenderer.java
+++ src/main/java/com/lunartyx/invasionmod/client/render/RiftZombiePiglinEntityRenderer.java
@@ -1,20 +1,30 @@
 package com.lunartyx.invasionmod.client.render;
 
 import com.lunartyx.invasionmod.InvasionMod;
+import com.lunartyx.invasionmod.entity.custom.RiftZombiePiglinEntity;
+
 import net.minecraft.client.render.entity.EntityRendererFactory;
-import net.minecraft.client.render.entity.ZombifiedPiglinEntityRenderer;
-import net.minecraft.entity.mob.ZombifiedPiglinEntity;
+import net.minecraft.client.render.entity.MobEntityRenderer;
+import net.minecraft.client.render.entity.model.PiglinEntityModel;
+import net.minecraft.client.render.entity.model.EntityModelLayers;
 import net.minecraft.util.Identifier;
 
-public class RiftZombiePiglinEntityRenderer extends ZombifiedPiglinEntityRenderer {
-    private static final Identifier TEXTURE = new Identifier(InvasionMod.MOD_ID, "textures/entity/rift_zombie_piglin.png");
+public class RiftZombiePiglinEntityRenderer
+        extends MobEntityRenderer<RiftZombiePiglinEntity, PiglinEntityModel<RiftZombiePiglinEntity>> {
+
+    private static final Identifier TEXTURE =
+            new Identifier(InvasionMod.MOD_ID, "textures/entity/rift_zombie_piglin.png");
 
     public RiftZombiePiglinEntityRenderer(EntityRendererFactory.Context context) {
-        super(context);
+        super(
+                context,
+                new PiglinEntityModel<>(context.getPart(EntityModelLayers.ZOMBIFIED_PIGLIN)),
+                0.5f // Schattenradius
+        );
     }
 
     @Override
-    public Identifier getTexture(ZombifiedPiglinEntity zombifiedPiglinEntity) {
+    public Identifier getTexture(RiftZombiePiglinEntity entity) {
         return TEXTURE;
     }
 }
